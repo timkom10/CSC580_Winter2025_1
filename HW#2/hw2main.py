910b9d55311547d3fe842fa25b693ebfa10d6312
Noriko Tomuro <ntomuro@users.noreply.github.com>
2025-01-13 12:19:22 -0600

    py file added

---
+++ HW#2/hw2main.py
@@ -0,0 +1,78 @@
+import gymnasium as gym
+import numpy as np
+
+from gymnasium.envs.toy_text.cliffwalking import CliffWalkingEnv
+from gymnasium.error import DependencyNotInstalled
+from os import path
+import time
+
+
+# Do not change this class
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+image_path = path.join(path.dirname(gym.__file__), "envs", "toy_text")
+
+
+def generate_random_policy(num_actions, num_states, seed=None):
+    """
+    A policy is a 1D array of length # of states, where each element is a
+    number between 0 (inclusive) and # of actions (exclusive) randomly chosen.
+    If a specific seed is passed, the same numbers are genereated, while
+    if the seed is None, the numbers are unpredictable every time.
+    """
+    rng = np.random.default_rng(seed)
+    return rng.integers(low=0, high=num_actions, size=num_states)
+    
+def run(env, pi, printinfo = False):
+    """
+    Run the policy on the environment and returns the cumulative reward.
+    :param: env: The environment
+    :param: pi: A given policy, represented as a 1D array of length # of states.
+    :return: Cumulative reward
+    """
+    s = env.reset()
+    if printinfo == True:
+      print (f'\n* Episode starting from state {s[0]}') # ensure starting from state 36
+
+    s = s[0]      # extract the state value/index from the tuple
+    done = False  # this becomes true when agent reaches the goal state (47)
+    sum_r = 0
+    while not done:
+        a = pi[s]   # action for the state s, according to the policy
+        s, r, done, info, p = env.step(a)  # take the action
+        sum_r += r  # accumulate reward
+
+        ### uncomment below to see the information for each step
+        #print (f'next_state={s}, reward={r}, done={done}, info={info}, p={p}')
+
+        # prints info in text if render_mode is 'ansi' or no output if 'human',
+        # or graphical output if 'rgb_array' AND if the code is run from command line.
+        env.render()
+    return sum_r
+    
+    
+# Create a Cliff Walk environment using Gymnasium
+env = gym.make("CliffWalking-v0", render_mode="rgb_array")#is_slippery=True, 'ansi')#
+
+# Reset the environment and display it
+env.reset()
+
+nS = env.observation_space.n    # number of states -- 48
+nA = env.action_space.n         # number of actions -- four directions; 0:left, 1:down, 2:right, 3:up
+print ("{}, {}".format(nS, nA))
+
+# Access the underlying environment using env.unwrapped
+env_unwrapped = env.unwrapped
+
+policy = generate_random_policy(nA, nS, 17) # third parameter is the random seed
+print ("*** Policy ***\n{}".format(policy.reshape((4, 12))))
+
+# Do just one run
+result = run(env, policy)
+# Print the total rewards/return
+print (f' ==> Total return: {result}')
+
+# Close the environment
+env.close()
